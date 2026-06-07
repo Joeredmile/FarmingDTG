efc0102029470148eb8811db7d1e240559fb0283
Joeredmile <joe.redmile@cathcollege.school.nz>
2026-06-08 10:28:50 +1200

    adding next growth

--- farming-dtg/scripts/carrot.gd
+++ farming-dtg/scripts/carrot.gd
@@ -2,6 +2,7 @@ extends AnimatedSprite2D
 
 @onready var timer: Timer = $Timer
 var carrot_watered = false
+var my_list = ["stage_3_a, stage_3_b"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,17 +18,13 @@ func _process(delta: float) -> void:
 func _on_timer_timeout():
 	if carrot_watered:
 		play("stage_2")
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		timer.stop()
+		carrot_watered = false
+
+func _on_timer_2_timeout() -> void:
+	if carrot_watered and animation == "stage_2":
+		play(my_list.pick_random())# Picks a random animation from the list
+		timer.stop()
+		carrot_watered = false
+	else:
+		pass
