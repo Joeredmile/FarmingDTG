cc73d8e65aec6cfabd11a8e81c68a3fd9df6bd56
Joeredmile <joe.redmile@cathcollege.school.nz>
2026-06-18 13:13:18 +1200

    vf

--- farming-dtg/scripts/npc.gd
+++ farming-dtg/scripts/npc.gd
@@ -42,7 +42,6 @@ func _process(delta):
 			NEW_DIR:
 				dir = choose([Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN])
 			MOVE:
-				print("npc_working")
 				move(delta)
 				
 func choose(array):
