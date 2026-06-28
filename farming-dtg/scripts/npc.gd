9e03f6213ac8dc6c7271e3c46979bb4833559d6a
Joeredmile <joe.redmile@cathcollege.school.nz>
2026-06-28 16:25:23 +1200

    ok

--- farming-dtg/scripts/npc.gd
+++ farming-dtg/scripts/npc.gd
@@ -1,8 +1,19 @@
 extends CharacterBody2D
 
+@onready var dialog_box = get_tree().get_first_node_in_group("dialog")
 
 const speed = 40
-var current_state = IDLE
+
+var dialog_lines = [
+	"Hello there!",
+	"Nice weather today.",
+	"Be careful out there.",
+    "I’ve been walking around all day."
+]
+
+var dialog_index = 0
+
+
 
 var dir = Vector2.RIGHT
 var start_pos
@@ -19,11 +30,17 @@ enum {
 	MOVE
 }
 
-func ready():
+var current_state = IDLE
+
+func _ready():
 	randomize()
 	start_pos = position
 func _process(delta):
-	if current_state == 0 or current_state == 1:
+	if player_in_chat_zone and Input.is_action_just_pressed("ui_accept"):
+		dialog_index = (dialog_index + 1) % dialog_lines.size()
+		show_dialog()
+	
+	if current_state == IDLE or current_state == NEW_DIR:
 		$AnimatedSprite2D.play("idle")
 	elif current_state == 2 and !is_chatting:
 		if dir.x == -1:
@@ -53,16 +70,30 @@ func move(delta):
 		position += dir * speed * delta
 
 
+func show_dialog():
+	if dialog_box:
+		dialog_box.get_node("DialogLabel").text = dialog_lines[dialog_index]
+		dialog_box.visible = true
+	else:
+		print("DialogBox not found in group 'dialog'")
+
+func hide_dialog():
+	if dialog_box:
+		dialog_box.visible = false
+
 func _on_chat_detection_area_body_entered(body: Node2D) -> void:
-	if body.has_method("player"):
+	if body.name == ("player"):
 		player = body
 		player_in_chat_zone = true
 		is_chatting = true
+		show_dialog()
 		
 
 func _on_chat_detection_area_body_exited(body: Node2D) -> void:
-	if body.has_method("player"):
+	if body.name == ("player"):
 		player_in_chat_zone = false
+		hide_dialog()
+
 
 func _on_timer_timeout() -> void:
 	$Timer.wait_time = choose([0.5, 1, 1.5])
