1d9efead066a7b55ecf20dd2ed10f73fa5ea0b71
Joeredmile <joe.redmile@cathcollege.school.nz>
2026-07-20 08:12:07 +1200

    deleted copy

--- farming-dtg-(copy)/scripts/GlobalData.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-@onready var level_1 = $"../level_1"
-@onready var player: CharacterBody2D = get_node("/root/level_1/player")
-@onready var bullet_timer: Timer = $bulletTimer
-
-var carrot_amount = 0
-var coin_amount = 0
-var collisons = false
-var carrot_seeds = 1
