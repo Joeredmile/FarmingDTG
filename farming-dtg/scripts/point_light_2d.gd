2f15ccadf8c47acc61d1c9249b1a348c164396c7
Joeredmile <joe.redmile@cathcollege.school.nz>
2026-07-28 10:29:43 +1200

    In descritption
    unfinished working on making the lights in the map turn off during the day and on at night time, also re added trees with cians new asset

--- farming-dtg/scripts/point_light_2d.gd
+++ farming-dtg/scripts/point_light_2d.gd
@@ -1,6 +1,6 @@
 extends PointLight2D
 
-@onready var canvas_modulate: CanvasModulate = get_node_or_null("level_1/CanvasModulate") as CanvasModulate
+@onready var canvas_modulate: CanvasModulate = $"../CanvasModulate"
 @onready var point_light_2d: PointLight2D = null
 
 const NIGHT_COLOR = Color("#0b213fff")
@@ -10,13 +10,23 @@ const TIME_SCALE = 0.005
 var light_visible = false
 
 func _ready() -> void:
-	pass
+	not visible
 
 func _process(delta: float) -> void:
 	if canvas_modulate == null:
 		return
-	visible = canvas_modulate.color.is_equal_approx(NIGHT_COLOR)
+	if canvas_modulate.color.is_equal_approx(NIGHT_COLOR):
+		light_visible = true
+		lighton()
+	if canvas_modulate.color.is_equal_approx(DAY_COLOR):
+		light_visible = false
+		lightoff()
 
 func lighton():
 	if light_visible == true:
 		visible = true
+		
+func lightoff():
+	if light_visible == false:
+		visible = false
+	
