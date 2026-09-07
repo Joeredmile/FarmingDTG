56a37ac7bd1e18c9e9ef3149ae3e36258a0ffcb1
Joeredmile <joe.redmile@cathcollege.school.nz>
2026-09-08 10:30:02 +1200

    Working on the rabbits and there day and night cycle

--- farming-dtg/scripts/rabbit.gd
+++ farming-dtg/scripts/rabbit.gd
@@ -4,17 +4,20 @@ class_name Enemy
 #@onready var carrot: AnimatedSprite2D = $"../carrot"
 const DEAD_COLOR = Color("#000000")
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var player: CharacterBody2D = $"../player"
+@onready var player: CharacterBody2D = $"player"
 var SPEED = 100.0
 @onready var timer: Timer = $Timer
 @onready var canvas_modulate: CanvasModulate = $CanvasModulate
 
 
-func _physics_process(delta: float) -> void:
-	if player == null: return
+func _process(delta: float) -> void:
+	#if player == null:
+	#	return
+	#else:
 	#where is the player
 	var direction = (player.global_position - global_position).normalized()
 	velocity = direction * SPEED
+	print("working")
 	move_and_slide()
 	if direction.x != 0:
 		$AnimatedSprite2D.flip_h = (direction.x < 0)
@@ -27,12 +30,6 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 			animated_sprite_2d.rotate(-71)
 			SPEED = 0
 			timer.start()
-
-
-			#canvas_modulate.color = Color.WHITE
-			#var tween = create_tween()
-			#tween.tween_property(canvas_modulate, "color", Color.BLACK, 3)
-
 
 func _on_timer_timeout() -> void:
 	queue_free()
